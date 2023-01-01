4e9b8aebffef6a0434f4c4e4b3d49386b8f5647a
kensiiwasaki <kensiiwasaki@gmail.com>
2023-01-01 23:28:54 +0900

    modify: 人口推移グラフ表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import { Graph } from 'src/components/Graph'
 import { PrefectureSelect } from 'src/components/PrefectureSelect'
 import { Layout } from 'src/layout'
+import { useFetchPopulationData } from 'src/lib/hooks/useFetchPopulationData'
+import { useGraphLabels } from 'src/lib/hooks/useGraphLabels'
+import { GraphData } from 'src/lib/type/graphData'
 
 import styles from '../styles/Home.module.css'
 
@@ -9,11 +13,36 @@ const Home: NextPage = () => {
   const [prefectureCheckedList, setPrefectureCheckedList] = useState<number[]>(
     []
   )
+  const { getGraphLabels, graphLabels, graphLabelsError } = useGraphLabels()
+  const { fetchPopulationData } = useFetchPopulationData()
+  const [graphDataList, setGraphDataList] = useState<GraphData[]>([])
+
+  const getPopulationData = useCallback(async () => {
+    const list = prefectureCheckedList.map((prefecture) => {
+      return fetchPopulationData(prefecture)
+    })
+    const dataList = await Promise.all(list)
+    setGraphDataList(dataList as GraphData[])
+  }, [prefectureCheckedList])
+
+  useEffect(() => {
+    getPopulationData()
+  }, [getPopulationData, prefectureCheckedList])
+
+  useEffect(() => {
+    getGraphLabels()
+  }, [])
+
+  if (graphLabelsError instanceof Error) {
+    return <div>{graphLabelsError.message}</div>
+  }
 
   return (
     <Layout>
       <PrefectureSelect setPrefectureCheckedList={setPrefectureCheckedList} />
-      <div>Hello</div>
+      <div>
+        <Graph datasets={graphDataList} labels={graphLabels} />
+      </div>
     </Layout>
   )
 }
